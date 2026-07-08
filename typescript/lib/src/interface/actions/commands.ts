0e43b942e9a76c2d6f70034ddae4dbe60bf8ed00
Corno Schraverus <corno@schraverus.com>
2026-07-08 07:31:12 +0200

    pdt: ...

--- typescript/lib/src/interface/actions/commands.ts
+++ typescript/lib/src/interface/actions/commands.ts
@@ -1,5 +1,5 @@
 import * as p_i from 'pareto-core/interface/command_action'
 
-import * as d_main from "./data/main.js"
+import * as d_main from "../data/main.js"
 
 export type main = p_i.Command_Action<d_main.Error, d_main.Parameters>
