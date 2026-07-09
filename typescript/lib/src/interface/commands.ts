75fc9688a0dd1052c787019590714b62670bfb9f
Corno Schraverus <corno@schraverus.com>
2026-07-09 16:32:51 +0200

    pdt: published version 0.1.4

---
+++ typescript/lib/src/interface/commands.ts
@@ -0,0 +1,8 @@
+import * as p_i from 'pareto-core/interface/command_interface'
+
+import type * as d_main from "./data/main.js"
+
+export type main = p_i.Command_Interface<
+    d_main.Error,
+    d_main.Parameters
+>
