5f0026c2a421209b88a1349efd50c71241c12878
Corno Schraverus <corno@schraverus.com>
2026-07-11 19:39:52 +0200

    pdt: ...

--- typescript/lib/src/interface/commands.ts
+++ typescript/lib/src/interface/commands.ts
@@ -1,8 +1,8 @@
 import * as p_i from 'pareto-core/interface/command_interface'
 
-import type * as d_main from "./schemas/main.js"
+import type * as s_main from "./schemas/main.js"
 
 export type main = p_i.Command_Interface<
-    d_main.Error,
-    d_main.Parameters
+    s_main.Error,
+    s_main.Parameters
 >
